840e4439e836b4ce4c96db698bb28d84887ee0c5
Robin Kahlow <tora@warlock.ai>
2021-11-28 22:07:48 +0000

    allow cli params for editor

--- AOEMods.Essence.Editor/App.xaml.cs
+++ AOEMods.Essence.Editor/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Microsoft.Toolkit.Mvvm.Messaging;
+using System.IO;
+using System.Windows;
 
 namespace AOEMods.Essence.Editor
 {
@@ -7,5 +9,14 @@ namespace AOEMods.Essence.Editor
     /// </summary>
     public partial class App : Application
     {
+        private void OnStartup(object sender, StartupEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            foreach (var arg in e.Args)
+            {
+                WeakReferenceMessenger.Default.Send(new OpenStreamMessage(File.OpenRead(arg), Path.GetExtension(arg)));
+            }
+            mainWindow.Show();
+        }
     }
 }
